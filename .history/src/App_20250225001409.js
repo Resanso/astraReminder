c659002c222599e438080cd5d5e54e1c8500baae
Sanzoo <saintsan196@gmail.com>
2025-02-25 00:18:11 +0700

    fix

--- .history/src/App_20250225001409.js
+++ .history/src/App_20250225001409.js
@@ -41,25 +41,8 @@ function App() {
         <header>
           <h1>Astra Domini</h1>
           {user && (
-            <button
-              className="logout-btn"
-              onClick={handleLogout}
-              title="Logout"
-            >
-              <svg
-                className="logout-icon"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-                <polyline points="16 17 21 12 16 7" />
-                <line x1="21" y1="12" x2="9" y2="12" />
-              </svg>
+            <button className="logout-btn" onClick={handleLogout}>
+              Logout
             </button>
           )}
         </header>
